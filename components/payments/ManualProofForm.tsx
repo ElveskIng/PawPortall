9c64c845a8fbb6922877209453812c94ce412499
ElveskIng <albiononline213@gmail.com>
2025-11-17 05:05:13 +0800

    inconsis

--- components/payments/ManualProofForm.tsx
+++ components/payments/ManualProofForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getSupabaseBrowserClient } from "@/lib/supabaseBrowser";
+import { getSupabaseBrowserClient } from "@/lib/supabaseClient";
 import { v4 as uuidv4 } from "uuid";
 
 type Proof = {
